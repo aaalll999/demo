98072b888365e9ae9930cf676a60daaee74f4a5c
aaalll999 <darshya290@gmail.com>
2025-11-11 23:36:23 +0530

    Update dsa5b.py
    Refactor Prim's algorithm implementation from a class-based structure to a functional approach. Update graph data and print format for minimum spanning tree output.

--- dsa5b.py
+++ dsa5b.py
@@ -1,50 +1,44 @@
 import sys
 
-class Graph:
-    def _init_(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for _ in range(vertices)] for _ in range(vertices)]
+V = 5 
+def min_key(key, mst_set):
+    min_val = sys.maxsize
+    min_index = -1
+    for v in range(V):
+        if key[v] < min_val and not mst_set[v]:
+            min_val = key[v]
+            min_index = v
+    return min_index
 
-    def print_mst(self, parent):
-        print("\nEdges in Minimum Spanning Tree:")
-        total = 0
-        for i in range(1, self.V):
-            print(f"Department {parent[i]} -- {i}  Distance = {self.graph[i][parent[i]]}")
-            total += self.graph[i][parent[i]]
-        print("Total Distance of MST:", total)
+def prim_mst(graph):
+    key = [sys.maxsize] * V
+    parent = [None] * V
+    key[0] = 0
+    mst_set = [False] * V
+    parent[0] = -1
 
-    def min_key(self, key, mstSet):
-        min_val = sys.maxsize
-        for v in range(self.V):
-            if key[v] < min_val and not mstSet[v]:
-                min_val = key[v]
-                min_index = v
-        return min_index
+    for _ in range(V - 1):
+        u = min_key(key, mst_set)
+        mst_set[u] = True
 
-    def prim_mst(self):
-        key = [sys.maxsize] * self.V
-        parent = [None] * self.V
-        key[0] = 0
-        mstSet = [False] * self.V
-        parent[0] = -1
+        for v in range(V):
+            if graph[u][v] and not mst_set[v] and graph[u][v] < key[v]:
+                key[v] = graph[u][v]
+                parent[v] = u
 
-        for _ in range(self.V):
-            u = self.min_key(key, mstSet)
-            mstSet[u] = True
-            for v in range(self.V):
-                if self.graph[u][v] and not mstSet[v] and self.graph[u][v] < key[v]:
-                    key[v] = self.graph[u][v]
-                    parent[v] = u
+    total_cost = 0
+    print("Edges in the Minimum Spanning Tree (Prim's Algorithm):")
+    for i in range(1, V):
+        print(f"{parent[i]} -- {i} == {graph[i][parent[i]]}")
+        total_cost += graph[i][parent[i]]
+    print("Minimum Spanning Tree Cost:", total_cost)
 
-        self.print_mst(parent)
-g = Graph(5)
-g.graph = [
-    [0, 10, 6, 5, 0],   
-    [10, 0, 0, 15, 0], 
-    [6, 0, 0, 4, 0],    
-    [5, 15, 4, 0, 8],   
-    [0, 0, 0, 8, 0]     
+graph = [
+    [0, 2, 8, 0, 0],
+    [2, 0, 5, 3, 0],
+    [8, 5, 0, 0, 6],
+    [0, 3, 0, 0, 0],
+    [0, 0, 6, 0, 0]
 ]
 
-g.prim_mst()
-
+prim_mst(graph)
